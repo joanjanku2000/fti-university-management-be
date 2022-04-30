dbd3e71bb6ed64117ef20bc121adc27c10813dbe
jjanku <janku.joan4@gmail.com>
2022-04-30 23:41:35 +0200

    Created Base Project

---
+++ src/main/java/al/edu/fti/universitymanagement/UniversityManagementApplication.java
@@ -0,0 +1,13 @@
+package al.edu.fti.universitymanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/al/edu/fti/universitymanagement/UniversityManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package al.edu.fti.universitymanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
