be27cb48c3a75c4f914dc2b8de2a5d1cf7ef1987
joanjanku2000 <janku.joan4@gmail.com>
2022-05-03 16:09:33 +0200

    MySQL Connnect

--- src/main/java/al/edu/fti/universitymanagement/api/course/CourseController.java
+++ src/main/java/al/edu/fti/universitymanagement/api/course/CourseController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1/course")
 public class CourseController extends BaseController<CourseDto, CourseEntity> {
+
     public CourseController(BaseService<CourseDto, CourseEntity> baseService) {
         super(baseService);
     }
