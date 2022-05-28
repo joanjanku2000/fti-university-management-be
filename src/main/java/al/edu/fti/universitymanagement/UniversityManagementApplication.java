012747efdddbdfb09ea8f126f3dd64ee25d48c2e
joanjanku2000 <janku.joan4@gmail.com>
2022-05-28 11:46:08 +0200

    Cross Origin

--- src/main/java/al/edu/fti/universitymanagement/UniversityManagementApplication.java
+++ src/main/java/al/edu/fti/universitymanagement/UniversityManagementApplication.java
@@ -3,6 +3,9 @@ package al.edu.fti.universitymanagement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
@@ -10,6 +13,17 @@ public class UniversityManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(UniversityManagementApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
