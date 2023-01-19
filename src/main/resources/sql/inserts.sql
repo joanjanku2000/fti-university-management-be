3e9918ab65eac2028215b120307ce0aee6c48a44
Joan Janku <janku.joan4@gmail.com>
2023-01-19 01:16:50 +0100

    inserts for postgres

--- src/main/resources/sql/inserts.sql
+++ src/main/resources/sql/inserts.sql
@@ -8,3 +8,12 @@ INSERT INTO fti_uniman.course (ID, name, description, start_time, end_time, acti
 INSERT INTO fti_uniman.course (ID, name, description, start_time, end_time, active, location_id) VALUES (6, 'Algebra', 'Algebra Coursee', '2022-07-08 15:02:06', '2022-07-08 16:02:06', 1, 6);
 INSERT INTO fti_uniman.university_user (ID, name, last_name, birthday, ID_NUMBER, gender, picture, role, registration_date, active, email, password) VALUES (1, 'Joan', 'Janku', '2000-02-04 00:00:00', '123456789', 0, null, 0, null, '1', 'jo@fti.edu.al', '$2a$10$do/3/rlrnQcZs/qHXeMdbOVDXrq.sv8aoCEjYdQTiU1LPqhCZpYdS');
 INSERT INTO fti_uniman.university_user (ID, name, last_name, birthday, ID_NUMBER, gender, picture, role, registration_date, active, email, password) VALUES (13, 'Stevi', 'Ziu', '2000-02-04 00:00:00', '123456789', 0, null, 0, null, '1', 'Stevi.Ziu@fti.edu.al', '$2a$10$AqyQI6pQoHSWRknMtVLBI.rrgnSbHmwmYzzA.ptxZRFPV9B7pLtJG');
+
+-- PostgreSQL
+INSERT INTO public.location (ID, name, active) VALUES (1, 'UPT HALL', true);
+INSERT INTO public.location (ID, name, active) VALUES (6, 'FSHN HALL', true);
+INSERT INTO public.course (ID, name, description, start_time, end_time, active, location_id) VALUES (1, 'Algebra', 'Course', '2022-07-08 15:02:06', '2022-07-08 16:02:06', false, 1);
+INSERT INTO public.course (ID, name, description, start_time, end_time, active, location_id) VALUES (2, 'Algebra', 'Algebra Coursee', '2022-07-08 15:02:06', '2022-07-08 16:02:06', false, 1);
+INSERT INTO public.course (ID, name, description, start_time, end_time, active, location_id) VALUES (6, 'Algebra', 'Algebra Coursee', '2022-07-08 15:02:06', '2022-07-08 16:02:06', true, 1);
+INSERT INTO public.university_user (ID, name, last_name, birthday, ID_NUMBER, gender, picture, role, registration_date, active, email, password) VALUES (1, 'Joan', 'Janku', '2000-02-04 00:00:00', '123456789', 0, null, 0, null, true, 'jo@fti.edu.al', '$2a$10$do/3/rlrnQcZs/qHXeMdbOVDXrq.sv8aoCEjYdQTiU1LPqhCZpYdS');
+INSERT INTO university_user (ID, name, last_name, birthday, ID_NUMBER, gender, picture, role, registration_date, active, email, password) VALUES (13, 'Stevi', 'Ziu', '2000-02-04 00:00:00', '123456789', 0, null, 0, null, true, 'Stevi.Ziu@fti.edu.al', '$2a$10$AqyQI6pQoHSWRknMtVLBI.rrgnSbHmwmYzzA.ptxZRFPV9B7pLtJG');
